63fa98d5abe8007e9a144cba56c2cf6ca9f2f204
Kevin <kmoens@lakeco.com>
2021-12-03 17:48:43 -0600

    initial version

---
+++ Scoreboard/Scoreboard/Server/Program.cs
@@ -0,0 +1,35 @@
+using Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("https://*.*","http://*.*")
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+
+app.MapHub<ScoreboardHub>("/hubs");
+
+
+app.Run();
---
+++ Scoreboard/Scoreboard/Server/ScoreboardHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server;
+public interface IScoreboardHub
+{
+    Task ScoreUpdate(string home, string away);
+}
+public class ScoreboardHub : Hub<IScoreboardHub>
+{
+    public async Task ScoreUpdate(string home, string away)
+    {
+        await Clients.All.ScoreUpdate(home, away);
+    }
+}
+
