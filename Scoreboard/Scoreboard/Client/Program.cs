91279a879fdd74f5044c19289e0847c133a0cec5
Kevin <kmoens@lakeco.com>
2021-12-03 18:08:47 -0600

    Add mudblazor

--- Scoreboard/Scoreboard/Client/Program.cs
+++ Scoreboard/Scoreboard/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Scoreboard.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,6 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
