cea8dafee061415a4075a501d712a60f04322faf
Kevin <kmoens@lakeco.com>
2021-12-05 16:10:30 -0600

    add helmets for each team.
    connect to server.ScoreboardHub for communication

---
+++ Scoreboard/Scoreboard/Client/Models/Teams.cs
@@ -0,0 +1,48 @@
+﻿namespace Scoreboard.Client.Models
+{
+    public class Teams : Dictionary<string, Team>
+    {
+        public Teams()
+        {
+            Add("New England", new Team() { Name = "New England", Abrev = "NE", ImageNum = "44" });
+            Add("Buffalo", new Team() { Name = "Buffalo", Abrev = "BUF", ImageNum = "61" });
+            Add("Miami", new Team() { Name = "Miami", Abrev = "MIA", ImageNum = "64" });
+            Add("NY Jets", new Team() { Name = "NY Jets", Abrev = "NYJ", ImageNum = "51" });
+            Add("Baltimore", new Team() { Name = "Baltimore", Abrev = "BAL", ImageNum = "10" });
+            Add("Cincinnati", new Team() { Name = "Cincinnati", Abrev = "CIN", ImageNum = "11" });
+            Add("Pittsburgh", new Team() { Name = "Pittsburgh", Abrev = "PIT", ImageNum = "12" });
+            Add("Cleveland", new Team() { Name = "Cleveland", Abrev = "CLE", ImageNum = "13" });
+            Add("Tennessee", new Team() { Name = "Tennessee", Abrev = "TEN", ImageNum = "14" });
+            Add("Indianapolis", new Team() { Name = "Indianapolis", Abrev = "IND", ImageNum = "50" });
+            Add("Houston", new Team() { Name = "Houston", Abrev = "HOU", ImageNum = "04" });
+            Add("Jacksonville", new Team() { Name = "Jacksonville", Abrev = "JAC", ImageNum = "53" });
+            Add("Kansas City", new Team() { Name = "Kansas City", Abrev = "KC", ImageNum = "30" });
+            Add("LA Chargers", new Team() { Name = "LA Chargers", Abrev = "LAC", ImageNum = "03" });
+            Add("Las Vegas", new Team() { Name = "Las Vegas", Abrev = "LV", ImageNum = "31" });
+            Add("Denver", new Team() { Name = "Denver", Abrev = "DEN", ImageNum = "52" });
+            Add("Dallas", new Team() { Name = "Dallas", Abrev = "DAL", ImageNum = "23" });
+            Add("Washington", new Team() { Name = "Washington", Abrev = "WAS", ImageNum = "21" });
+            Add("Philadelphia", new Team() { Name = "Philadelphia", Abrev = "PHI", ImageNum = "22" });
+            Add("NY Giants", new Team() { Name = "NY Giants", Abrev = "NYG", ImageNum = "20" });
+            Add("Green Bay", new Team() { Name = "Green Bay", Abrev = "GB", ImageNum = "24" });
+            Add("Minnesota", new Team() { Name = "Minnesota", Abrev = "MIN", ImageNum = "33" });
+            Add("Chicago", new Team() { Name = "Chicago", Abrev = "CHI", ImageNum = "32" });
+            Add("Detroit", new Team() { Name = "Detroit", Abrev = "DET", ImageNum = "62" });
+            Add("Tampa Bay", new Team() { Name = "Tampa Bay", Abrev = "TB", ImageNum = "43" });
+            Add("Atlanta", new Team() { Name = "Atlanta", Abrev = "ATL", ImageNum = "34" });
+            Add("Carolina", new Team() { Name = "Carolina", Abrev = "CAR", ImageNum = "41" });
+            Add("New Orleans", new Team() { Name = "New Orleans", Abrev = "NO", ImageNum = "40" });
+            Add("Arizona", new Team() { Name = "Arizona", Abrev = "ARZ", ImageNum = "00" });
+            Add("LA Rams", new Team() { Name = "LA Rams", Abrev = "LAR", ImageNum = "42" });
+            Add("San Francisco", new Team() { Name = "San Francisco", Abrev = "SF", ImageNum = "02" });
+            Add("Seattle", new Team() { Name = "Seattle", Abrev = "SEA", ImageNum = "01" });
+        }
+    }
+    public class Team
+    {
+        public string? Name { get; set; }
+        public string? Abrev { get; set; }
+        public string? ImageNum { get; set; }
+        public int Score { get; set; }
+    }
+}
--- Scoreboard/Scoreboard/Server/ScoreboardHub.cs
+++ Scoreboard/Scoreboard/Server/ScoreboardHub.cs
@@ -4,12 +4,22 @@ namespace Server;
 public interface IScoreboardHub
 {
     Task ScoreUpdate(string home, string away);
+    Task HomeUpdate(string home);
+    Task AwayUpdate(string away);
 }
 public class ScoreboardHub : Hub<IScoreboardHub>
 {
     public async Task ScoreUpdate(string home, string away)
     {
         await Clients.All.ScoreUpdate(home, away);
+    }
+    public async Task HomeUpdate(string home)
+    {
+        await Clients.All.HomeUpdate(home);
+    }
+    public async Task AwayUpdate(string away)
+    {
+        await Clients.All.AwayUpdate(away);
     }
 }
 
